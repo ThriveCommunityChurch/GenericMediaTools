a30aa5317dce2d1b27b59534df5576696b488cd9
Wyatt Baggett <wyattbaggett@yahoo.com>
2021-05-17 23:17:05 -0400

    Init Dupe Cleaner tool

---
+++ DupeCleaner/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DupeCleaner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
