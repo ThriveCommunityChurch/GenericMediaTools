1c59de13c0c6c24e15f424564c1be9d07a14737a
Wyatt Baggett <wyattbaggett@yahoo.com>
2021-05-22 00:32:23 -0400

    Add NuGet pkgs and get project ready to start development work

---
+++ DupeCleaner/AppSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace DupeCleaner
+{
+    public class AppSettings
+    {
+       /// <summary>
+       /// Whether or not to enforce uniqueness across similar image extensions (specifically .jpg/.jpeg)
+       /// </summary>
+       public string EnforceImageExtensions { get; set; }
+    }
+}
--- DupeCleaner/Program.cs
+++ DupeCleaner/Program.cs
@@ -6,7 +6,7 @@ namespace DupeCleaner
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
